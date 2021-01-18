34a848495b38af35660fb65b77af27539a097f3a
shikha <roy.shikha579@gmail.com>
2021-01-18 23:11:48 +0530

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
-function App() {
+export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+  const [display, setDisplay] = React.useState([]);
+  const [editableText, setEditableText] = React.useState("");
+
+  function isBlank(str) {
+    str = str.trim();
+    return !str || 0 === str.length || !Boolean(str);
+  }
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function handleAdd() {
+    if (!isBlank(inputText)) {
+      setItems((prevItems) => {
+        return [...prevItems, inputText];
+      });
+    }
+    let arr = [...display];
+    arr.push(false);
+    setDisplay(arr);
+    setInputText("");
+    // setButtonType("Add");
+  }
+
+  function handleDelete(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+    let arr = [...display];
+    arr.splice(id, 1);
+    setDisplay(arr);
+  }
+
+  function handleEdit(id) {
+    let arr = [...display];
+    arr[id] = true;
+    setDisplay(arr);
+    //console.log(display);
+    setEditableText(items[id]);
+  }
+
+  function handleEditChange(event) {
+    // editableText = event.target.value;
+    setEditableText(event.target.value);
+    console.log(editableText);
+  }
+
+  function handleSave(id) {
+    if (!isBlank(editableText)) {
+      let arr = [...display];
+      arr[id] = false;
+      setDisplay(arr);
+      let textArr = [...items];
+      textArr[id] = editableText;
+      setItems(textArr);
+      setEditableText("");
+    }
+  }
+
+  function handleCancel(id) {
+    let arr = [...display];
+    arr[id] = false;
+    setDisplay(arr);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div className="container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+        <InputArea
+          handleChange={handleChange}
+          addItem={handleAdd}
+          inputText={inputText}
+        />
+        <div>
+          <ul className="pl-25">
+            {items.map((todoItem, index) => (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onDelete={handleDelete}
+                onEdit={handleEdit}
+                onEditChange={handleEditChange}
+                onShow={display[index]}
+                onSave={handleSave}
+                checkSaveButton={editableText}
+                onCancel={handleCancel}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/ToDoItem.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+  function handleClick() {
+    setIsDone((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <>
+      <div
+        className="list"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+          {props.text.substring(0, 30)}
+        </li>
+        <div>
+          <div style={{ display: "flex", lineHeight: "2.3" }}>
+            <span
+              className="pointer edit"
+              onClick={() => props.onEdit(props.id)}
+              role="img"
+              aria-label="done"
+            >
+              <i className="fa fa-pencil editIcon" aria-hidden="true"></i>
+              <button>Edit</button>
+            </span>
+            <span
+              className="pointer"
+              onClick={handleClick}
+              role="img"
+              aria-label="done"
+            >
+              <i className="fa fa-thumb-tack pinIcon" aria-hidden="true"></i>
+              <button>Cancel</button>
+            </span>
+            <span
+              className="pointer delete"
+              onClick={() => props.onDelete(props.id)}
+              role="img"
+              aria-label="delete"
+            >
+              <i className="fa fa-trash deleteIcon" aria-hidden="true"></i>
+              <button>Delete</button>
+            </span>
+          </div>
+        </div>
+      </div>
+      {props.onShow && (
+        <div>
+          <input
+            type="text"
+            className="editTask"
+            onChange={props.onEditChange}
+          />
+          <span
+            className="saveTask pointer"
+            onClick={() => props.onSave(props.id)}
+            disabled={!props.checkSaveButton}
+            role="img"
+            aria-label="done"
+          >
+            ✅
+          </span>
+          <span
+            className="cancelTask pointer"
+            onClick={() => props.onCancel(props.id)}
+            disabled={!props.checkSaveButton}
+            role="img"
+            aria-label="done"
+          >
+              ❌
+          </span>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ToDoItem;
